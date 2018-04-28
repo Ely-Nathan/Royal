e5bf4abe1194c169407441708185904343ede0fb
Elijah Nathan <ely.nathan93@gmail.com>
2018-04-28 14:41:41 +0100

    Add Bootstrap SCSS and style booking form

--- _scripts/scripts.js
+++ _scripts/scripts.js
@@ -1,12 +1,36 @@
-$(function () {
-    $('#datetimepicker4').datetimepicker({
-      format: 'ddd Mo MMM, YYYY',
-      extraFormats: [ 'YYYY-MM-DD' ],
-      locale: 'uk',
-      allowInputToggle: true, // Minimum date is today's date
-      minDate: new Date(), // Dont use current date when opening
-      useCurrent: true
-    });
+$(function() {
+  $('.date').datetimepicker({
+    format: 'DD-MM-YYYY',
+    locale: 'uk',
+    allowInputToggle: true, // Minimum date is today's date
+    defaultDate: new Date(),
+    minDate: new Date(), // Dont use current date when opening
+    useCurrent: true
+  });
 });
-var now = moment()
-console.log(typeof moment.defineLocale)
+
+window.setInterval(function(){
+  var d1 = document.getElementById("checkInDisplay").value;
+  if(d1 !== null) {
+    var year = d1.substring(6,10);
+    var month = d1.substring(3,5);
+    var day = d1.substring(0,2);
+    newDate = year + "-" + month + "-" + day;
+    document.getElementById("checkIn").value = newDate;
+  }
+  else {
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; //January is 0!
+
+    var yyyy = today.getFullYear();
+    if(dd<10){
+        dd='0'+dd;
+    }
+    if(mm<10){
+        mm='0'+mm;
+    }
+    var today = dd+'/'+mm+'/'+yyyy;
+    document.getElementById("DATE").value = today;
+    }
+  }, 500);
